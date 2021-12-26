4ee6bef24b012ca05eeb22924a298b4a9b323991
Ahmet Basli <ahmetbasli@ogoodigital.com>
2021-12-26 21:35:28 +0300

    Fix multer

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
-var path = require('path');
+let fs = require('fs-extra');
+const Product = require("../servers/product-service");
 
 const multer = require('multer');
 
 const storage = multer.diskStorage({
-    dest: 'uploads/' 
+    destination: (req, file, callback) => {
+        const path = `./public/uploads/`;
+        fs.mkdirsSync(path);
+        callback(null, path);
+      },
+      filename: (req, file, callback) => {
+        //originalname is the uploaded file's name with extn
+        callback(null,`${new Date().toISOString().replace(/:/g, '-')}-${file.originalname}` );
+      }
+        
 });
 
 const fileFilter = (req, file, cb) => {
@@ -18,15 +28,15 @@ const fileFilter = (req, file, cb) => {
 };
 
 const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5
-  },
-  fileFilter: fileFilter
+    storage: storage,
+    limits: {
+      fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter 
 });
 
-router.post("/", upload.single('img'),  async (req, res) => {
-   console.log(req.file)
+router.post("/", upload.single('productImage'),  async (req, res) => {
+    
   });
   
   router.get("/", async (req, res) => {
