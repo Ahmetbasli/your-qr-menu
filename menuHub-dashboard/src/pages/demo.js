31cb69082c97c75b4e794ec7fbfe737942459402
Ahmet Basli <ahmetbasli@ogoodigital.com>
2021-12-28 01:58:01 +0300

    Add components

--- menuHub-dashboard/src/pages/demo.js
+++ menuHub-dashboard/src/pages/demo.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 
 // components
-// import CategoryFeed from '../components/CategoryFeed/CategoryFeed'
-// import Header from '../components/Header/Header'
-// import Title from '../components/Title/Title'
+import CategoryFeed from '../components/CategoryFeed/CategoryFeed'
+import Header from '../components/Header/Header'
+import Title from '../components/Title/Title'
 //styles
 import styles from '../styles/pages/Demo.module.css'
 
@@ -11,9 +11,9 @@ const Demo = ({ data: categories={} }) => {
     return (
         <main className={styles.main}>
           hello
-          {/* <Header/>
+          <Header/>
           <Title/>
-          <CategoryFeed categories={categories}/>  */}
+          <CategoryFeed categories={categories}/> 
         </main>
     )
 }
